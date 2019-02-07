5939696930bbe264301fc8227fae25ca7f4df3fa
Renat Ishkaev <rishkaev@gotrg.com>
2019-02-07 11:46:18 +0300

    - added project files

---
+++ Gf.RabbitMq.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gf.RabbitMq.Example
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
